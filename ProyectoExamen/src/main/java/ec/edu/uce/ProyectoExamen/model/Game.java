ad652833e2d033f71a856630e99a9329c3e7c5ef
EdgarTipan <evtipanl1@uce.edu.ec>
2024-06-01 10:26:31 -0500

    Proyecto agregado

---
+++ ProyectoExamen/src/main/java/ec/edu/uce/ProyectoExamen/model/Game.java
@@ -0,0 +1,4 @@
+package ec.edu.uce.ProyectoExamen.model;
+
+public class Game {
+}
---
+++ ProyectoExamen/src/test/java/ec/edu/uce/ProyectoExamen/ProyectoExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.ProyectoExamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
